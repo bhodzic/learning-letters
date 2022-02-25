8799b54e3ca55557144338a82300bb9c35294610
bhodzic <66036368+bhodzic@users.noreply.github.com>
2022-02-25 17:32:34 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,68 @@
+
+const latinica = ['A', 'B', 'C', 'Č', 'Ć', 'D', 'Dž', 'Đ', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'LJ', 'M', 'N', 'NJ', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
+const cirilica = ['А', 'Б', 'В', 'Г', 'Д', 'Ђ', 'Е', 'Ж', 'З', 'И', 'Ј', 'К', 'Л', 'Љ', 'М', 'Н', 'Њ', 'О', 'П', 'Р', 'С', 'Т', 'Ћ', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Џ', 'Ш'];
+const alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const animals = [{
+    latinica: 'KONJ',
+    cirilica: 'КОЊ',
+    alfabet: 'HORSE'
+}, {
+    latinica: 'PAS',
+    cirilica: 'ПАС',
+    alfabet: 'DOG'
+}, {
+    latinica: 'MAJMUN',
+    cirilica: 'МАЈМУН',
+    alfabet: 'MONKEY'
+}, {
+    latinica: 'ZEC',
+    cirilica: 'ЗЕЦ',
+    alfabet: 'RABBIT'
+}, {
+    latinica: 'SOVA',
+    cirilica: 'СОВА',
+    alfabet: 'OWL'
+}, {
+    latinica: 'PANDA',
+    cirilica: 'ПАНДА',
+    alfabet: 'PANDA'
+}, {
+    latinica: 'MEDVED',
+    cirilica: 'МЕДВЕД',
+    alfabet: 'BEAR'
+}, {
+    latinica: 'ŽABA',
+    cirilica: 'ЖАБА',
+    alfabet: 'FROG'
+},
+{
+    latinica: 'TIGAR',
+    cirilica: 'ТИГАР',
+    alfabet: 'TIGER'
+},
+{
+    latinica: 'SLON',
+    cirilica: 'СЛОН',
+    alfabet: 'ELEPHANT'
+},
+{
+    latinica: 'KOALA',
+    cirilica: 'КОАЛА',
+    alfabet: 'KOALA'
+},
+{
+    latinica: 'LAV',
+    cirilica: 'ЛАВ',
+    alfabet: 'LION'
+},
+{
+    latinica: 'SVINJA',
+    cirilica: 'СВИЊА',
+    alfabet: 'PIG'
+},
+{
+    latinica: 'LISICA',
+    cirilica: 'ЛИСИЦА',
+    alfabet: 'FOX'
+}];
